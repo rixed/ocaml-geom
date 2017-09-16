30038531ec47072e8bf1ecc73a58918e958dfc74
Cedric Cellier <rixed@happyleptic.org>
2017-09-16 19:25:56 +0200

    More retabbing

--- text_intf.ml
+++ text_intf.ml
@@ -2,27 +2,27 @@ type orientation = Vertical | Horizontal
 
 module type GLYPH =
 sig
-	module Poly : Geom.POLYGON
-	module Path : Geom.PATH with module Point = Poly.Point
-	type t
+  module Poly : Geom.POLYGON
+  module Path : Geom.PATH with module Point = Poly.Point
+  type t
 
-	val make     : char -> t
-	val to_paths : t -> Path.t list
-	val to_polys : t -> Path.Point.K.t -> Poly.t list
-	val bbox     : t -> Poly.Point.Bbox.t
-	val advance  : ?orientation:orientation -> t -> t -> Poly.Point.t
-(*	val line_gap : t -> Path.scalar *)
+  val make     : char -> t
+  val to_paths : t -> Path.t list
+  val to_polys : t -> Path.Point.K.t -> Poly.t list
+  val bbox     : t -> Poly.Point.Bbox.t
+  val advance  : ?orientation:orientation -> t -> t -> Poly.Point.t
+(*  val line_gap : t -> Path.scalar *)
 end
 
 module type WORD =
 sig
-	module Glyph : GLYPH
+  module Glyph : GLYPH
 
-	type t
+  type t
 
-	val make     : ?orientation:orientation -> string -> t
-	val bbox     : t -> Glyph.Poly.Point.Bbox.t
-	val to_paths : t -> (Glyph.Poly.Point.t * Glyph.Path.t list) list
-	val to_polys : t -> Glyph.Path.Point.K.t -> (Glyph.Poly.Point.t * Glyph.Poly.t list) list
-	(* [to_polys word prec] returns a list of offset*polys *)
+  val make     : ?orientation:orientation -> string -> t
+  val bbox     : t -> Glyph.Poly.Point.Bbox.t
+  val to_paths : t -> (Glyph.Poly.Point.t * Glyph.Path.t list) list
+  val to_polys : t -> Glyph.Path.Point.K.t -> (Glyph.Poly.Point.t * Glyph.Poly.t list) list
+  (* [to_polys word prec] returns a list of offset*polys *)
 end
